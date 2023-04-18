bdaa2c3e9b380c30853cbde92bf41d3e87a4ea77
Ashling <114626446+AshlingnLog@users.noreply.github.com>
2023-04-17 23:52:27 -0500

    working on a build

--- bad-bank-context/navbar.js
+++ bad-bank-context/navbar.js
@@ -1,36 +1,30 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+function NavBar(){
+  return(
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+              <a class="nav-link" href="home.js">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="createaccount.js">Create Account</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="deposit.js">Deposit</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="withdraw.js">Withdraw</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="alldata.js">All Data</a>
       </li>
     </ul>
   </div>
 </nav>
-
-function NavBar(){
-  return(
-    <>
-    <a href="#">BadBank</a>,
-    <a href="#/CreateAccount/">Create Account</a>,
-    <a href="#/login/">Login</a>,
-    <a href="#/deposit/">Deposit</a>,
-    <a href="#/withdraw/">Withdraw</a>,
-    <a href="#/balance/">Balance</a>,
-    <a href="#/alldata/">AllData</a>
     </>
   );
 }
