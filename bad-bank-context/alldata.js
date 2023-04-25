a22132c0a6006627f2d0af41fc0ba3d455241ffa
Ashling <114626446+AshlingnLog@users.noreply.github.com>
2023-04-25 10:07:48 -0500

    updated HTTP to HTTPS

--- bad-bank-context/alldata.js
+++ bad-bank-context/alldata.js
@@ -5,3 +5,34 @@ function AllData(){
     </>
   );
 }
+import React, { useState } from 'react';
+import { Card, CardBody, CardTitle } from 'reactstrap';
+
+function AllData() {
+  const [data, setData] = useState([]);
+
+  // function to add data to the state
+  const addData = (newData) => {
+    setData([...data, newData]);
+  }
+
+  // map over the data and create a list of entries
+  const dataList = data.map((entry, index) => {
+    return (
+      <li key={index}>
+        {entry.type}: {entry.amount}
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <Card>
+        <CardBody>
+          <CardTitle tag="h3">All Data</CardTitle>
+          <ul>{dataList}</ul>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
--- bad-bank-context/deposit.js
+++ bad-bank-context/deposit.js
@@ -3,3 +3,79 @@ function Deposit(){
     <h1>Deposit</h1>
   )
 }
+
+import React, { useState } from "react";
+import { Card, Form, Button, Alert } from "react-bootstrap";
+
+const DepositPage = () => {
+  const [balance, setBalance] = useState(0);
+  const [depositAmount, setDepositAmount] = useState("");
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showNaNAlert, setShowNaNAlert] = useState(false);
+  const [showNegativeAlert, setShowNegativeAlert] = useState(false);
+
+  const handleDeposit = (e) => {
+    e.preventDefault();
+    if (isNaN(depositAmount)) {
+      setShowNaNAlert(true);
+      return;
+    }
+    if (depositAmount < 0) {
+      setShowNegativeAlert(true);
+      return;
+    }
+    setBalance((prevBalance) => prevBalance + parseFloat(depositAmount));
+    setDepositAmount("");
+    setShowSuccess(true);
+  };
+
+  const handleDepositAmountChange = (e) => {
+    setDepositAmount(e.target.value);
+  };
+
+  const handleAlertClose = () => {
+    setShowNaNAlert(false);
+    setShowNegativeAlert(false);
+    setShowSuccess(false);
+  };
+
+  return (
+    <Card>
+      <Card.Header>Deposit Page</Card.Header>
+      <Card.Body>
+        {showSuccess && (
+          <Alert variant="success" onClose={handleAlertClose} dismissible>
+            Deposit received successfully!
+          </Alert>
+        )}
+        {showNaNAlert && (
+          <Alert variant="danger" onClose={handleAlertClose} dismissible>
+            Please enter a valid number.
+          </Alert>
+        )}
+        {showNegativeAlert && (
+          <Alert variant="danger" onClose={handleAlertClose} dismissible>
+            Please enter a positive number.
+          </Alert>
+        )}
+        <Card.Title>Balance: {balance}</Card.Title>
+        <Form onSubmit={handleDeposit}>
+          <Form.Group controlId="formDepositAmount">
+            <Form.Label>Deposit Amount</Form.Label>
+            <Form.Control
+              type="text"
+              value={depositAmount}
+              onChange={handleDepositAmountChange}
+              placeholder="Enter deposit amount"
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" disabled={!depositAmount}>
+            Deposit
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default DepositPage;
